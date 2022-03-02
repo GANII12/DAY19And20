9b0ff172e1a758ff1fa792998b66a32600fa6691
GANII12 <ganesh.17je003174@ece.ism.ac.in>
2022-03-02 16:07:26 +0530

    updating

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -11,7 +11,7 @@ public class UserRegistration {
         System.out.println(matcher.find());
 
         //UC2 LastName
-        System.out.println("------------UC2-----------");
+        System.out.println("-----------UC2-----------");
 
         Pattern lastName = Pattern.compile("^[A-Z][a-z]{3,}$");
         Matcher matcher1 = lastName.matcher("Moturu");
@@ -25,10 +25,34 @@ public class UserRegistration {
         System.out.println(matcher2.find());
 
         //UC4 MobileFormat
-        System.out.println("-----------UC3------------");
+        System.out.println("-----------UC4------------");
         Pattern mobile = Pattern.compile("^(\\+91|0|91)?[9876][0-9]{9}$");
         Matcher matcher3 = mobile.matcher("09949975752");
         System.out.println(matcher3.find());
+
+        //UC5 PASSWORD - minimum 8 characters
+        System.out.println("-----------UC5------------");
+        Pattern password = Pattern.compile("[A-Z a-z 0-9]{8,}");
+        Matcher matcher4 = password.matcher("awefvfveqr");
+        System.out.println(matcher4.find());
+
+        //UC6 PASSWORD - should have at least 1 Uppercase
+        System.out.println("-----------UC6------------");
+        Pattern password1 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z]){8}.*$");
+        Matcher matcher5 = password1.matcher("rBuskbefhghA");
+        System.out.println(matcher5.find());
+
+        //UC7 PASSWORD - should have at least 1 numeric number
+        System.out.println("-----------UC7------------");
+        Pattern password2 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]){8,}.*$");
+        Matcher matcher6 = password2.matcher("1ab461354D1ijP");
+        System.out.println(matcher6.find());
+
+        //UC8 PASSWORD - should have at least 1 numeric number
+        System.out.println("-----------UC8------------");
+        Pattern password3 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]){8,}.*$");
+        Matcher matcher7 = password3.matcher("1ab+461354D@1ijP-");
+        System.out.println(matcher7.find());
 
 
     }
